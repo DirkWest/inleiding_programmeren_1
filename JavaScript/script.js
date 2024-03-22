b1921c90466ed86c66632d4998914ac69304a94a
Dirk Westerbeek <151652104+DirkWest@users.noreply.github.com>
2024-03-22 13:02:57 +0100

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,11 @@
+console.log ("Test123")
+
+let afbeelding = document.getElementById("auto=wit,lamp=geel.svg");
+
+afbeelding.addEventListener("click", function() {
+    if (image.scr.endsWith("auto=wit,lamp=geel.svg")) {
+        img.src = "auto=blauw,lamp=geel.svg"
+    } else {
+        image.src = "auto=wit,lamp=geel.svg"
+    }
+});
